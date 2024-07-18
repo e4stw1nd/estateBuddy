05f3ac3e42b1d893f136884c45e665997a1d1f77
Ankur Roy <e4stw7nd@gmail.com>
2024-07-18 18:36:00 +0530

    Updated reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,7 +4,7 @@ const router = express.Router({ mergeParams: true });
 const Estate = require('../models/estate');
 const Review = require('../models/review');
 
-const { reviewSchema } = require('../utils/schemas.js');
+const { reviewSchema } = require('../utils/schemaVerify');
 
 
 const AppError = require('../utils/AppError');
